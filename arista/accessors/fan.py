742875c10390dd19f031a635becae6d3d319459a
Zhi Yuan Carl Zhao <zyzhao@arista.com>
2020-06-23 18:15:14 -0700

    Add logging for fans when speed set to/reduced from max
    Change-Id: Icf2dc7b089131ef281e89b24a873311a3c88eaa0

--- arista/accessors/fan.py
+++ arista/accessors/fan.py
@@ -1,10 +1,17 @@
+from ..core.log import getLogger
 from ..inventory.fan import Fan
 
+logging = getLogger(__name__)
+
 class FanImpl(Fan):
+   MIN_FAN_SPEED = 30
+   MAX_FAN_SPEED = 100
+
    def __init__(self, fanId=1, driver=None, led=None, **kwargs):
       self.fanId = fanId
       self.driver = driver
       self.led = led
+      self.customSpeed = None
       self.__dict__.update(kwargs)
 
    def getId(self):
@@ -17,6 +24,11 @@ class FanImpl(Fan):
       return self.driver.getFanSpeed(self)
 
    def setSpeed(self, speed):
+      if self.customSpeed == self.MAX_FAN_SPEED and speed != self.MAX_FAN_SPEED:
+         logging.debug("%s fan speed reduced from max", self.getName())
+      elif self.customSpeed != self.MAX_FAN_SPEED and speed == self.MAX_FAN_SPEED:
+         logging.warn("%s fan speed set to max", self.getName())
+      self.customSpeed = speed
       return self.driver.setFanSpeed(self, speed)
 
    def getPresence(self):
