86832e63426a900673691d09084c61d0c56cba34
Samuel Angebault <staphylo@arista.com>
2020-08-27 11:03:48 -0700

    scd-hwmon: Add uart implementation
    Add the core logic of the uart controller.
    
    Change-Id: Ifaad729802a8cb8a3fe7d76d0ef5450db118e65f

--- src/scd-uart.c
+++ src/scd-uart.c
@@ -16,13 +16,363 @@
  */
 
 #include <linux/module.h>
+#include <linux/tty.h>
+#include <linux/tty_flip.h>
 
 #include "scd.h"
 #include "scd-hwmon.h"
 #include "scd-uart.h"
 
+/*
+ * helpers
+ */
+
+#define scd_invalid_reg(_reg) ((_reg).reg ==  0xffffffff)
+
+static inline struct scd_uart_port *to_scd_uart_port(struct uart_port *port)
+{
+   return container_of(port, struct scd_uart_port, port);
+}
+
+static inline struct scd_context *to_scd_ctx(struct scd_uart_port *port)
+{
+   return container_of(port->uart, struct scd_context, uart);
+}
+
+static inline u32 scd_read_uart_port(struct scd_uart_port *port, u32 offset)
+{
+   return scd_read_register(to_scd_ctx(port)->pdev, offset);
+}
+
+static inline void scd_write_uart_port(struct scd_uart_port *port, u32 offset,
+                                       u32 val)
+{
+   scd_write_register(to_scd_ctx(port)->pdev, offset, val);
+}
+
+static inline union uart_rx_ctl scd_read_rx_ctl(struct scd_uart_port *port)
+{
+   return (union uart_rx_ctl) {
+      .reg = scd_read_uart_port(port, SCD_UART_RX_CTL_ADDR(port))
+   };
+}
+
+static inline void scd_write_rx_ctl(struct scd_uart_port *port,
+                                    union uart_rx_ctl ctl)
+{
+   scd_write_uart_port(port, SCD_UART_RX_CTL_ADDR(port), ctl.reg);
+}
+
+static inline union uart_rx_sm_ctl scd_read_rx_sm_ctl(struct scd_uart_port *port)
+{
+   return (union uart_rx_sm_ctl) {
+      .reg = scd_read_uart_port(port, SCD_UART_RX_SM_CTL_ADDR(port))
+   };
+}
+
+static inline union uart_rx_sm_res scd_read_rx_sm_res(struct scd_uart_port *port)
+{
+   return (union uart_rx_sm_res) {
+      .reg = scd_read_uart_port(port, SCD_UART_RX_SM_RES_ADDR(port))
+   };
+}
+
+static inline union uart_tx_ctl scd_read_tx_ctl(struct scd_uart_port *port)
+{
+   return (union uart_tx_ctl) {
+      .reg = scd_read_uart_port(port, SCD_UART_TX_CTL_ADDR(port))
+   };
+}
+
+static inline void scd_write_tx_ctl(struct scd_uart_port *port,
+                                    union uart_tx_ctl ctl)
+{
+   scd_write_uart_port(port, SCD_UART_TX_CTL_ADDR(port), ctl.reg);
+}
+
+static inline union uart_tx_sm_req scd_read_tx_sm_req(struct scd_uart_port *port)
+{
+   return (union uart_tx_sm_req) {
+      .reg = scd_read_uart_port(port, SCD_UART_TX_SM_REQ_ADDR(port))
+   };
+}
+
+static inline void scd_write_tx_sm_req(struct scd_uart_port *port,
+                                       union uart_tx_sm_req req)
+{
+   scd_write_uart_port(port, SCD_UART_TX_SM_REQ_ADDR(port), req.reg);
+}
+
+static inline union uart_tx_sm_res scd_read_tx_sm_res(struct scd_uart_port *port)
+{
+   return (union uart_tx_sm_res) {
+      .reg = scd_read_uart_port(port, SCD_UART_TX_SM_RES_ADDR(port))
+   };
+}
+
+#define uart_prefix(_func, _uart, _fmt, _args...)            \
+   _func(&to_scd_ctx(to_scd_uart_port(_uart))->pdev->dev,    \
+         "UART #%u: " _fmt, to_scd_uart_port(_uart)->id, ##_args)
+#define uart_dbg(_uart, _fmt, _args...)                       \
+   uart_prefix(dev_dbg, _uart, _fmt, ##_args)
+#define uart_notice(_uart, _fmt, _args...)                    \
+   uart_prefix(dev_notice, _uart, _fmt, ##_args)
+#define uart_warn(_uart, _fmt, _args...)                      \
+   uart_prefix(dev_warn, _uart, _fmt " (%s:%d)", ##_args, __func__, __LINE__)
+#define uart_err(_uart, _fmt, _args...)                      \
+   uart_prefix(dev_err, _uart, _fmt " (%s:%d)", ##_args, __func__, __LINE__)
+
+/*
+ * uart ops
+ */
+
+static unsigned int scd_uart_tx_empty(struct uart_port *port)
+{
+   struct scd_uart_port *sp = to_scd_uart_port(port);
+   union uart_tx_sm_res res = scd_read_tx_sm_res(sp);
+
+   return (res.nrs >= SCD_UART_FIFO_DEPTH) ? 0 : TIOCSER_TEMT;
+}
+
+static void scd_uart_set_mctrl(struct uart_port *port, unsigned int mctrl)
+{
+}
+
+static unsigned int scd_uart_get_mctrl(struct uart_port *port)
+{
+   return 0;
+}
+
+static void scd_uart_stop_tx(struct uart_port *port)
+{
+   uart_dbg(port, "stop tx\n");
+}
+
+static void scd_uart_transmit_chars(struct uart_port *port)
+{
+   struct circ_buf *xmit = &port->state->xmit;
+   struct scd_uart_port *sp = to_scd_uart_port(port);
+   union uart_tx_sm_res res = scd_read_tx_sm_res(sp);
+   union uart_tx_sm_req req = { .reg = 0 };
+
+   uart_dbg(port, "transmit chars pending=%ld\n", uart_circ_chars_pending(xmit));
+
+   req.push = 1;
+   req.st = 1;
+
+   while (res.nrs < SCD_UART_FIFO_DEPTH) {
+      if (port->x_char) {
+         req.d = port->x_char;
+         scd_write_tx_sm_req(sp, req);
+         port->icount.tx++;
+         port->x_char = 0;
+      }
+
+      if (uart_circ_empty(xmit) || uart_tx_stopped(port))
+         break;
+
+      req.d = xmit->buf[xmit->tail];
+      scd_write_tx_sm_req(sp, req);
+      xmit->tail = (xmit->tail + 1) % UART_XMIT_SIZE;
+      port->icount.tx++;
+
+      res.nrs++;
+   }
+
+   if (uart_circ_chars_pending(xmit) < WAKEUP_CHARS)
+      uart_write_wakeup(port);
+
+   if (uart_circ_empty(xmit))
+      scd_uart_stop_tx(port);
+}
+
+static void scd_uart_receive_chars(struct uart_port *port)
+{
+   struct tty_port *tp = &port->state->port;
+   struct scd_uart_port *sp = to_scd_uart_port(port);
+   union uart_rx_sm_res res = scd_read_rx_sm_res(sp);
+
+   uart_dbg(port, "receive chars %u\n", res.nrs);
+   sp->no_rx_since = res.nrs ? 1 : min(sp->no_rx_since + 1, SCD_UART_FIFO_DEPTH / 2);
+
+   while (!res.fe && !scd_invalid_reg(res)) {
+      port->icount.rx++;
+      tty_insert_flip_char(tp, res.d, TTY_NORMAL);
+      res = scd_read_rx_sm_res(sp);
+   }
+
+   tty_flip_buffer_push(tp);
+}
+
+static void scd_uart_start_tx(struct uart_port *port)
+{
+   uart_dbg(port, "start tx\n");
+   scd_uart_transmit_chars(port);
+}
+
+static void scd_uart_stop_rx(struct uart_port *port)
+{
+   uart_dbg(port, "stop rx\n");
+}
+
+static void scd_uart_break_ctl(struct uart_port *port, int ctl)
+{
+}
+
+static void scd_uart_port_do_work(struct scd_uart_port *sp)
+{
+   queue_work(sp->workqueue, &sp->work);
+}
+
+#define UART_AVG_BITS_PER_BYTE 10
+static ktime_t scd_uart_poll_interval(struct scd_uart_port *port)
+{
+   unsigned bits_delay = UART_AVG_BITS_PER_BYTE * port->no_rx_since;
+   port->poll_interval = ktime_set(0, NSEC_PER_SEC / (port->baud / bits_delay));
+   return port->poll_interval;
+}
+
+static enum hrtimer_restart scd_uart_port_timer_callback(struct hrtimer *timer)
+{
+   struct scd_uart_port *sp = container_of(timer, struct scd_uart_port, timer);
+   uart_dbg(&sp->port, "timer callback interval=%lld\n", sp->poll_interval);
+   scd_uart_port_do_work(sp);
+   hrtimer_forward(timer, timer->base->get_time(), scd_uart_poll_interval(sp));
+   return HRTIMER_RESTART;
+}
+
+static void scd_uart_port_work_callback(struct work_struct *work)
+{
+   struct scd_uart_port *sp = container_of(work, struct scd_uart_port, work);
+   scd_uart_receive_chars(&sp->port);
+}
+
+static int scd_uart_startup(struct uart_port *port)
+{
+   struct scd_uart_port *sp = to_scd_uart_port(port);
+   char buffer[16];
+
+   uart_dbg(port, "startup\n");
+   if (sp->workqueue) {
+      uart_warn(port, "workqueue already in use\n");
+      return -EBUSY;
+   }
+
+   sprintf(buffer, "scd-uart-%u", sp->id);
+   sp->workqueue = create_freezable_workqueue(buffer);
+   if (!sp->workqueue) {
+      uart_err(port, "failed to create workqueue");
+      return -EBUSY;
+   }
+   INIT_WORK(&sp->work, scd_uart_port_work_callback);
+
+   hrtimer_init(&sp->timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+   hrtimer_start(&sp->timer, sp->poll_interval, HRTIMER_MODE_REL);
+   sp->timer.function = scd_uart_port_timer_callback;
+
+   return 0;
+}
+
+static void scd_uart_shutdown(struct uart_port *port)
+{
+   struct scd_uart_port *sp = to_scd_uart_port(port);
+
+   uart_dbg(port, "shutdown\n");
+
+   if (sp->workqueue) {
+      flush_workqueue(sp->workqueue);
+      destroy_workqueue(sp->workqueue);
+      sp->workqueue = NULL;
+   }
+   hrtimer_cancel(&sp->timer);
+}
+
+static u8 scd_uart_brs_for_baud(speed_t baud) {
+   static const speed_t rates[] = { 4800, 9600, 19200, 38400, 57600, 115200,
+                                    230400, 460800, 921600, 1843200 };
+   int i;
+
+   for (i = 0; i < ARRAY_SIZE(rates); i++)
+      if (baud == rates[i])
+         return i;
+
+   return 0xff;
+}
+
+static void scd_uart_set_termios(struct uart_port *port,
+                                 struct ktermios *new,
+                                 struct ktermios *old)
+{
+   struct scd_uart_port *sp = to_scd_uart_port(port);
+   union uart_rx_ctl rx_ctl = scd_read_rx_ctl(sp);
+   union uart_tx_ctl tx_ctl = scd_read_tx_ctl(sp);
+   speed_t baud;
+   u8 parity;
+   u8 stb;
+   u8 brs;
+
+   uart_dbg(port, "set_termios\n");
+
+   baud = tty_termios_baud_rate(new);
+   brs = scd_uart_brs_for_baud(baud);
+   if (brs == 0xff) {
+      baud = sp->baud;
+      brs = scd_uart_brs_for_baud(baud);
+   }
+   sp->baud = baud;
+
+   tty_termios_encode_baud_rate(new, baud, baud);
+
+   scd_uart_poll_interval(sp);
+
+   rx_ctl.brs = brs;
+   tx_ctl.brs = brs;
+
+   stb = (new->c_cflag & CSTOPB) ? 2 : 1;
+   rx_ctl.stb = stb;
+   tx_ctl.stb = stb;
+
+   parity = (new->c_cflag & PARENB) ? 1 : 0;
+   rx_ctl.parity = parity;
+   tx_ctl.parity = parity;
+
+   // mask missing features
+   new->c_cflag &= ~(CMSPAR | PARODD);
+
+   scd_write_rx_ctl(sp, rx_ctl);
+   scd_write_tx_ctl(sp, tx_ctl);
+
+   uart_update_timeout(port, new->c_cflag, baud);
+}
+
+static const char *scd_uart_type(struct uart_port *port)
+{
+   return "scd";
+}
+
+static void scd_uart_config_port(struct uart_port *port, int flags)
+{
+   uart_dbg(port, "config port flags=%d\n", flags);
+}
+
+static struct uart_ops scd_uart_ops = {
+   .tx_empty = scd_uart_tx_empty,
+   .set_mctrl = scd_uart_set_mctrl,
+   .get_mctrl = scd_uart_get_mctrl,
+   .stop_tx = scd_uart_stop_tx,
+   .start_tx = scd_uart_start_tx,
+   .stop_rx = scd_uart_stop_rx,
+   .break_ctl = scd_uart_break_ctl,
+   .startup = scd_uart_startup,
+   .shutdown = scd_uart_shutdown,
+   .set_termios = scd_uart_set_termios,
+   .type = scd_uart_type,
+   .config_port = scd_uart_config_port,
+};
+
 static void scd_uart_remove_port(struct scd_uart_port *port)
 {
+   uart_dbg(&port->port, "removing\n");
    uart_remove_one_port(&port->uart->driver, &port->port);
    list_del(&port->list);
    kfree(port);
@@ -44,9 +394,6 @@ void scd_uart_remove_all(struct scd_context *ctx)
    uart_unregister_driver(&uart->driver);
    uart->initialized = false;
 }
-
-static struct uart_ops scd_uart_ops = {
-};
 
 static int scd_uart_maybe_initialize(struct scd_context *ctx)
 {
@@ -96,6 +443,8 @@ int scd_uart_add(struct scd_context *ctx, u32 addr, u32 id)
    port->addr_tx = addr + SCD_UART_TX_ADDR_OFFSET;
    port->id = id;
    port->baud = 9600;
+   port->no_rx_since = 1;
+   scd_uart_poll_interval(port);
 
    spin_lock_init(&port->port.lock);
    port->port.dev = get_scd_dev(ctx);
--- src/scd-uart.h
+++ src/scd-uart.h
@@ -36,10 +36,18 @@ struct scd_uart_port {
    struct list_head list;
 
    struct uart_port port;
+
+   // XXX: primitive replacement of irqs
+   struct workqueue_struct *workqueue;
+   struct work_struct work;
+   struct hrtimer timer;
+   ktime_t poll_interval;
+
    u32 addr_rx;
    u32 addr_tx;
    u32 id;
    speed_t baud;
+   int no_rx_since;
 };
 
 #define PORT_SCD 42 // Value available in include/uapi/linux/serial_core.h
@@ -49,6 +57,89 @@ struct scd_uart_port {
 #define SCD_UART_RX_ADDR_OFFSET 0
 #define SCD_UART_TX_ADDR_OFFSET 0x100
 
+#define SCD_UART_RX_CTL_OFFSET    0x0
+#define SCD_UART_RX_SM_CTL_OFFSET 0x4
+#define SCD_UART_RX_SM_RES_OFFSET 0x8
+
+#define SCD_UART_TX_CTL_OFFSET    0x0
+#define SCD_UART_TX_SM_REQ_OFFSET 0x4
+#define SCD_UART_TX_SM_RES_OFFSET 0x8
+
+#define SCD_UART_RX_CTL_ADDR(Port) (((Port)->addr_rx) + SCD_UART_RX_CTL_OFFSET)
+#define SCD_UART_RX_SM_CTL_ADDR(Port) (((Port)->addr_rx) + SCD_UART_RX_SM_CTL_OFFSET)
+#define SCD_UART_RX_SM_RES_ADDR(Port) (((Port)->addr_rx) + SCD_UART_RX_SM_RES_OFFSET)
+
+#define SCD_UART_TX_CTL_ADDR(Port) (((Port)->addr_tx) + SCD_UART_TX_CTL_OFFSET)
+#define SCD_UART_TX_SM_REQ_ADDR(Port) (((Port)->addr_tx) + SCD_UART_TX_SM_REQ_OFFSET)
+#define SCD_UART_TX_SM_RES_ADDR(Port) (((Port)->addr_tx) + SCD_UART_TX_SM_RES_OFFSET)
+
+union uart_rx_ctl {
+   u32 reg;
+   struct {
+      u32 reserved1:7;
+      u32 debugs:9;
+      u32 reserved2:4;
+      u32 parity:1;
+      u32 reserved3:3;
+      u32 brs:4;
+      u32 stb:2;
+      u32 reserved4:2;
+   } __packed;
+};
+
+union uart_rx_sm_ctl {
+   u32 reg;
+   struct {
+      u32 reserved1:24;
+      u32 intc:8;
+   } __packed;
+};
+
+union uart_rx_sm_res {
+   u32 reg;
+   struct {
+      u32 d:8;
+      u32 reserved1:8;
+      u32 nrs:8;
+      u32 reserved2:7;
+      u32 fe:1;
+   } __packed;
+};
+
+union uart_tx_ctl {
+   u32 reg;
+   struct {
+      u32 reserved1:7;
+      u32 debugs:9;
+      u32 reserved2:4;
+      u32 parity:1;
+      u32 reserved3:3;
+      u32 brs:4;
+      u32 stb:2;
+      u32 reserved4:2;
+   } __packed;
+};
+
+union uart_tx_sm_req {
+   u32 reg;
+   struct {
+      u32 d:8;
+      u32 reserved1:19;
+      u32 push:1;
+      u32 reserved2:3;
+      u32 st:1;
+   } __packed;
+};
+
+union uart_tx_sm_res {
+   u32 reg;
+   struct {
+      u32 reserved1:16;
+      u32 nrs:7;
+      u32 reserved2:8;
+      u32 fe:1;
+   } __packed;
+};
 
 extern int scd_uart_add(struct scd_context *ctx, u32 addr, u32 id);
 extern void scd_uart_remove_all(struct scd_context *ctx);
