16ed7220ee6ad2460883cfe3bab52afdf01c0694
Samuel Angebault <staphylo@arista.com>
2020-12-15 10:42:13 -0800

    Add Hedgehog CPU temp sensor
    Change-Id: Ib4cff7bb6bcbdc749608dee228830e49cc0f70c0

--- arista/platforms/cpu/hedgehog.py
+++ arista/platforms/cpu/hedgehog.py
@@ -2,8 +2,11 @@ from ...core.cpu import Cpu
 from ...core.register import Register, RegisterMap
 from ...core.types import I2cAddr, PciAddr
 
+from ...components.cpu.amd.k10temp import K10Temp
 from ...components.eeprom import PrefdlSeeprom
 from ...components.scd import Scd
+
+from ...descs.sensor import SensorDesc, Position
 
 class HedgehogCpuCpld(RegisterMap):
    REVISION = Register(0x10, name='revision')
@@ -18,6 +21,10 @@ class HedgehogCpu(Cpu):
    def __init__(self, slot, **kwargs):
       super(HedgehogCpu, self).__init__(**kwargs)
       self.slot = slot
-      self.eeprom = self.newComponent(PrefdlSeeprom, I2cAddr(1, 0x50))
-      self.syscpld = self.newComponent(Scd, PciAddr(bus=0, device=0x18, func=7),
+      self.eeprom = self.newComponent(PrefdlSeeprom, addr=I2cAddr(1, 0x50))
+      self.syscpld = self.newComponent(Scd, addr=PciAddr(device=0x18, func=7),
                                        registerCls=HedgehogCpuCpld)
+      self.newComponent(K10Temp, addr=PciAddr(device=0x18, func=3), sensors=[
+         SensorDesc(diode=0, name='Cpu temp sensor',
+                    position=Position.OTHER, target=60, overheat=90, critical=95),
+      ])
