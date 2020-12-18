aa6cd6db471a285d929a600106b33de58dab9486
Samuel Angebault <staphylo@arista.com>
2020-12-18 11:28:30 -0800

    Add unit test for chassis
    Change-Id: Ie33728547751c59c8bfb99d94ad321cbfb619ca7

---
+++ arista/components/denali/tests/chassis.py
@@ -0,0 +1,66 @@
+
+from ....core.utils import incrange
+
+from ....platforms.chassis.camp import Camp
+from ....platforms.chassis.northface import NorthFace
+from ....platforms.fabric.brooks import Brooks
+from ....platforms.fabric.eldridge import Eldridge
+from ....platforms.fabric.dragonfly import Dragonfly
+from ....platforms.linecard.clearwater import Clearwater, ClearwaterMs
+from ....platforms.linecard.clearwater2 import Clearwater2, Clearwater2Ms
+from ....platforms.supervisor.otterlake import OtterLake
+
+from ....tests.testing import unittest
+
+class DenaliChassisTest(unittest.TestCase):
+   def _buildChassis(self, chassis, supervisors, fabrics, linecards):
+      chassis = chassis()
+      for slotId, cls in supervisors.items():
+         sup = cls(chassis=chassis, slot=None)
+         sup.slotId = slotId
+         chassis.insertSupervisor(sup, slotId=slotId, active=slotId == 1)
+
+      def hookCardSlots(coll, inv):
+         for i, slot in enumerate(coll, 1):
+            cls = inv.get(i)
+            if cls is None:
+               slot.getPresence = lambda: False
+            else:
+               slot.getEeprom = lambda c=cls: { 'SKU': c.SKU[0] }
+
+      hookCardSlots(chassis.active.fabricSlots, fabrics)
+      hookCardSlots(chassis.active.linecardSlots, linecards)
+
+      chassis.loadLinecards()
+      chassis.loadFabrics()
+
+      return chassis
+
+   def _buildBasicChassis(self, chassis, supervisor, fabric):
+      return self._buildChassis(
+         chassis,
+         supervisors={
+            1: supervisor,
+         },
+         fabrics={
+            slotId: fabric for slotId in incrange(1, 6)
+         },
+         linecards={
+            1: Clearwater,
+            2: ClearwaterMs,
+            3: Clearwater2,
+            4: Clearwater2Ms,
+         },
+      )
+
+   def testCampChassis(self):
+      self._buildBasicChassis(Camp, OtterLake, Brooks)
+
+   def testNorthFaceEldridgeChassis(self):
+      self._buildBasicChassis(NorthFace, OtterLake, Eldridge)
+
+   def testNorthFaceDragonflyChassis(self):
+      self._buildBasicChassis(NorthFace, OtterLake, Dragonfly)
+
+if __name__ == '__main__':
+   unittest.main()
