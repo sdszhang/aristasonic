8737d72e0aa59f7c51829ff3d60c15f421e1b61e
Samuel Angebault <staphylo@arista.com>
2020-12-22 16:13:40 -0800

    Improve arista show platform environment
    Change-Id: I56729862abd29ccd2dcf663974e90b7684349c9e

--- arista/cli/show/environment.py
+++ arista/cli/show/environment.py
@@ -1,4 +1,8 @@
 
+from __future__ import print_function
+
+from ...core.diag import DiagContext
+
 from . import Renderer
 
 class ShowEnvironment(Renderer):
@@ -6,21 +10,55 @@ class ShowEnvironment(Renderer):
       super(ShowEnvironment, self).__init__('environment')
 
    def data(self, show):
-      data = []
+      ctx = DiagContext()
+      data = {
+         'fans': [],
+         'temps': [],
+         'psus': [],
+      }
       for inventory, _ in show.inventories:
-         prefix = '' # FIXME inventory name required
          for temp in inventory.getTemps():
-            data.append({
-               'name': '%s%s' % (prefix, temp.sensor.name),
-               'temp': temp.getTemperature(),
-               'target': temp.sensor.target,
-            })
+            data['temps'].append(temp.__diag__(ctx))
+         for fan in inventory.getFans():
+            data['fans'].append(fan.__diag__(ctx))
+         for psu in inventory.getPsus():
+            data['psus'].append(psu.__diag__(ctx))
       return data
 
+   def _renderCollection(self, hdr, collection):
+      if not collection:
+         return
+
+      fmt = ' '.join('%%-%ds' % sz for _, _, sz in hdr)
+      print(fmt % tuple(n for n, _, _ in hdr))
+      print(fmt % tuple('-' * sz for _, _, sz in hdr))
+      for item in collection:
+         print(fmt % tuple(item[k] for _, k, _ in hdr))
+
    def renderText(self, show):
-      for sensor in self.data(show):
-         print('%-50s %-8s %-6s' % (
-            sensor['name'],
-            sensor['temp'],
-            sensor['target'],
-         ))
+      data = self.data(show)
+
+      tempHdr = [
+         ('Name', 'name', 40),
+         ('Temp', 'value', 8),
+         ('Alert', 'highThresh', 8),
+         ('Critical', 'highCritThresh', 8),
+      ]
+      fanHdr = [
+         ('Name', 'name', 10),
+         ('Model', 'model', 15),
+         ('Status', 'status', 6),
+         ('Speed', 'speed', 4),
+      ]
+      psuHdr = [
+         ('Name', 'name', 10),
+         ('Model', 'model', 15),
+         ('Serial', 'serial', 15),
+         ('Status', 'status', 6),
+      ]
+
+      self._renderCollection(tempHdr, data['temps'])
+      print()
+      self._renderCollection(fanHdr, data['fans'])
+      print()
+      self._renderCollection(psuHdr, data['psus'])
