98f0ea5f996517dee653d02c36cb11e243a9a2dc
Baptiste Covolato <baptiste@arista.com>
2021-02-19 16:58:37 -0800

    daemon/storage: add iostat collection
    Change-Id: Ide13e0c87c56d7a2611cb62efb11b949db869294

--- arista/daemon/storage.py
+++ arista/daemon/storage.py
@@ -6,10 +6,75 @@ from ..core.log import getLogger
 
 logging = getLogger(__name__)
 
+SECTOR_SIZE = 512
+SECONDS_IN_DAY = 60 * 60 * 24
+KiB = 1024
+MiB = 1024 * KiB
+
+class IOStat(object):
+   def __init__(self, deviceName):
+      self.deviceName = deviceName
+      self.readBytes = -1
+      self.writtenBytes = -1
+      self.oldReadBytes = -1
+      self.oldWrittenBytes = -1
+      self.uptime = -1
+
+   def __str__(self):
+      if self.readBytes == -1 or self.writtenBytes == -1 or self.uptime == -1:
+         return '(unknown)'
+
+      totalReadStr = 'total data read (MiB): %.1f' % (self.readBytes / MiB)
+      totalWrittenStr = 'total data written (MiB): %.1f' % \
+                        (self.writtenBytes / MiB)
+
+      if self.oldReadBytes > -1:
+         totalReadStr += ' (+%.1f)' % ((self.readBytes - self.oldReadBytes) / MiB)
+
+      if self.oldWrittenBytes > -1:
+         totalWrittenStr += ' (+%.1f)' % \
+                            ((self.writtenBytes - self.oldWrittenBytes) / MiB)
+
+      totalReadKiB = self.readBytes / KiB
+      totalWrittenKiB = self.writtenBytes / KiB
+      output = [
+         totalReadStr,
+         totalWrittenStr,
+         'read (KiB/s): %.1f' % (totalReadKiB / self.uptime),
+         'write (KiB/s): %.1f' % (totalWrittenKiB / self.uptime),
+      ]
+
+      uptimeDays = self.uptime / SECONDS_IN_DAY
+      if uptimeDays >= 1:
+         totalReadMiB = self.readBytes / MiB
+         totalWrittenMiB = self.writtenBytes / MiB
+         output += [
+            'read (MiB/day): %.1f' % (totalReadMiB / uptimeDays),
+            'write (MiB/day): %.1f' % (totalWrittenMiB / uptimeDays),
+         ]
+
+      return ', '.join(output)
+
+   def collect(self):
+      with open('/proc/uptime') as f:
+         self.uptime = float(f.read().split()[0])
+
+      # https://www.kernel.org/doc/Documentation/block/stat.txt
+      statPath = '/sys/block/%s/stat' % self.deviceName
+      with open(statPath) as f:
+         statData = f.read().split()
+
+      self.oldReadBytes = self.readBytes
+      self.oldWrittenBytes = self.writtenBytes
+
+      self.readBytes = int(statData[2]) * SECTOR_SIZE
+      self.writtenBytes = int(statData[6]) * SECTOR_SIZE
+
 class StorageDevice(object):
    def __init__(self, name, path):
       self.name = name
       self.path = path
+      self.stat = IOStat(self.name)
 
    def __str__(self):
       return '%s(%s)' % (self.__class__.__name__, self.name)
@@ -25,6 +90,14 @@ class StorageDevice(object):
 
    def exists(self):
       return os.path.exists(self.path)
+
+   def iostat(self):
+      try:
+         self.stat.collect()
+      except Exception: # pylint: disable=broad-except
+         return
+
+      logging.info('iostat(%s): %s', self.name, self.stat)
 
    @classmethod
    def listStorageDevices(cls):
@@ -102,7 +175,7 @@ class UsbStorageDevice(StorageDevice):
 class StorageDaemonFeature(PollDaemonFeature):
 
    NAME = 'storage'
-   INTERVAL = 12 * 60 * 60
+   INTERVAL = 24 * 60 * 60
 
    STORAGE_DEVICES = [
       EmmcStorageDevice,
@@ -126,3 +199,4 @@ class StorageDaemonFeature(PollDaemonFeature):
       for device in self.devices:
          if device.exists():
             device.report()
+            device.iostat()
