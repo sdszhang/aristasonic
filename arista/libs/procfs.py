6c9bf3a37dd166a0ac7c68d114cb5898d0358941
Samuel Angebault <staphylo@arista.com>
2021-06-15 06:47:51 -0700

    testing: Fix uptime when /proc/uptime does not exist
    Some build environments have no access to a working procfs leading
    to tests failing as per the following issue.
    https://github.com/Azure/sonic-buildimage/issues/7800
    
    Change-Id: Ie3f0ca957281d978ff5fb669982ec77013783b72

--- arista/libs/procfs.py
+++ arista/libs/procfs.py
@@ -1,11 +1,17 @@
 
 import datetime
+import time
 
 from .fs import readFileContent
 
 def uptime(path='/proc/uptime'):
    '''Read uptime from /proc/uptime'''
-   return tuple(float(v) for v in readFileContent(path).rstrip().split())
+   try:
+      return tuple(float(v) for v in readFileContent(path).rstrip().split())
+   except IOError:
+      # NOTE: This codepath should only be reached during testing in environments
+      #       where the procfs is not reachable.
+      return (float(time.time()), float(time.time()))
 
 def bootDatetime():
    '''Read uptime and return a datetime object representing boot time'''
