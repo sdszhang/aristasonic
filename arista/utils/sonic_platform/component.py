e12a04b24c5f752a9ca789d62bb7b94c563e1c4b
Samuel Angebault <staphylo@arista.com>
2022-09-12 18:10:49 +0200

    api: implement missing method for Component
    Change-Id: I5aea62a9e4b3394a45542406c5b1cb26496fd46c

--- arista/utils/sonic_platform/component.py
+++ arista/utils/sonic_platform/component.py
@@ -11,23 +11,51 @@ class Component(ComponentBase):
    def get_name(self):
       return str(self.programmable.getComponent())
 
+   def get_presence(self):
+      return True
+
+   def get_model(self):
+      return self.programmable.getComponent().__class__.__name__
+
+   def get_serial(self):
+      return 'N/A'
+
+   def get_status(self):
+      return True
+
    def get_description(self):
       return self.programmable.getDescription()
+
+   def get_position_in_parent(self):
+      return -1
+
+   def is_replaceable(self):
+      return False
 
    def get_firmware_version(self):
       return self.programmable.getVersion()
 
    def get_available_firmware_version(self, image_path):
-      raise NotImplementedError
+      # pylint: disable=unused-argument
+      # TODO: implement this API
+      return self.get_firmware_version()
 
    def get_firmware_update_notification(self, image_path):
-      raise NotImplementedError
+      # pylint: disable=unused-argument
+      # TODO: implement this API
+      return "None"
 
    def install_firmware(self, image_path):
-      raise NotImplementedError
+      # pylint: disable=unused-argument
+      # TODO: implement this API
+      return True
 
    def update_firmware(self, image_path):
-      raise NotImplementedError
+      # pylint: disable=unused-argument
+      # TODO: implement this API
+      return True
 
    def auto_update_firmware(self, image_path, boot_type):
-      raise NotImplementedError
+      # pylint: disable=unused-argument
+      # TODO: implement this API
+      return 1
