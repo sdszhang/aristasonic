592a5c97909f36d0218791c8fc0415af5c8329c6
Samuel Angebault <staphylo@arista.com>
2022-11-23 08:59:27 +0100

    wolverine: update power draw information
    Change-Id: I05a79cfdd6783a36810ee789cce9b0b8291f89ae

--- arista/platforms/linecard/wolverine.py
+++ arista/platforms/linecard/wolverine.py
@@ -208,14 +208,12 @@ class WolverineQCpu(WolverineQ):
    TYP_POWER_DRAW = 563
 
 @registerPlatform()
-class WolverineQCpuBk(WolverineQ):
-   SID = ['WolverineQCpuBk']
-   SKU = ['7800R3AK-36D2-LC']
-
-@registerPlatform()
 class WolverineQCpuMs(WolverineQ):
    SID = ['WolverineQCpuMs']
    SKU = ['7800R3A-36DM2-LC']
+
+   MAX_POWER_DRAW = 794
+   TYP_POWER_DRAW = 578
 
 @registerPlatform()
 class WolverineQCpuBkMs(WolverineQ):
