b01ba29c0374b7cebc6c9ff9309f0eb2e26b6232
Cheryl Sanchez <cheryls@arista.com>
2023-05-17 14:06:44 -0700

    multi-asic: Fix processing of port_config.ini.
    Change-Id: I87d2d3f102ff2add9d89751ce677a01cb07a2872

--- arista/utils/sonic_utils.py
+++ arista/utils/sonic_utils.py
@@ -17,29 +17,31 @@ def parsePortConfig():
    '''
    portMapping = {}
 
-   with open(portConfigPath()) as fp:
-      header = fp.readline()[1:].split()
-      headerMap = {key.strip(): idx for (idx, key) in enumerate(header)}
-      for line in fp:
-         line = line.strip()
-         if not line or line[0] == '#':
-            continue
+   portConfigPathList = getPortConfigPaths()
+   for portConfigFile in portConfigPathList:
+      with open(portConfigFile) as fp:
+         header = fp.readline()[1:].split()
+         headerMap = {key.strip(): idx for (idx, key) in enumerate(header)}
+         for line in fp:
+            line = line.strip()
+            if not line or line[0] == '#':
+               continue
 
-         fields = line.split()
-         # "portNum" is determined from the "index" column or derived from the
-         # "alias" column.
-         # "lanes" is determined from the number of lanes in the "lanes" column.
-         # "offset" is determined from the second number of the "alias" column.
-         # "singular" is determined by if the alias has a '/' character or not.
-         name = fields[headerMap['name']]
-         lanes = len(fields[headerMap['lanes']].split(','))
-         alias = fields[headerMap['alias']]
-         aliasRe = re.findall(r'\d+', alias)
-         portNum = int(aliasRe[0])
-         singular = True if len(aliasRe) < 2 else False
-         offset = 0 if singular else (int(aliasRe[1]) - 1)
+            fields = line.split()
+            # "portNum" is determined from the "index" column or derived from the
+            # "alias" column.
+            # "lanes" is determined from the number of lanes in the "lanes" column.
+            # "offset" is determined from the second number of the "alias" column.
+            # "singular" is determined by if the alias has a '/' character or not.
+            name = fields[headerMap['name']]
+            lanes = len(fields[headerMap['lanes']].split(','))
+            alias = fields[headerMap['alias']]
+            aliasRe = re.findall(r'\d+', alias)
+            portNum = int(aliasRe[0])
+            singular = True if len(aliasRe) < 2 else False
+            offset = 0 if singular else (int(aliasRe[1]) - 1)
 
-         portMapping[name] = Port(portNum, lanes, offset, singular, alias)
+            portMapping[name] = Port(portNum, lanes, offset, singular, alias)
 
    return portMapping
 
@@ -60,14 +62,38 @@ def getSonicHwSkuName():
    hwSkuKey = "DEVICE_METADATA['localhost']['hwsku']"
    return getSonicConfigVar(hwSkuKey)
 
-def portConfigPath():
-   CONTAINER_PORT_CONFIG_PATH = "/usr/share/sonic/hwsku/port_config.ini"
+def getPlatformPath():
    if runningInContainer():
-      return CONTAINER_PORT_CONFIG_PATH
-   hwSku = getSonicHwSkuName()
-   platform = getSonicPlatformName()
-   return os.path.join("/usr/share/sonic/device", platform, hwSku,
-                        "port_config.ini")
+      return "/usr/share/sonic/platform"
+   return os.path.join("/usr/share/sonic/device/", getSonicPlatformName())
+
+def getHwSkuPath():
+   if runningInContainer():
+      return "/usr/share/sonic/hwsku"
+   return os.path.join(getPlatformPath(), getSonicHwSkuName())
+
+def getPortConfigPaths():
+   '''
+   Create and return a list of files to read that contain the
+   port configuration information. Currently that is one or more
+   port_config.ini files. For the multi-asic linecard there may be
+   a single file or one for each asic.
+   In the future this information may be obtained from the files
+   platform.json and/or hwsku.json.
+   '''
+   portConfigPathList = []
+   hwSkuPath = getHwSkuPath()
+   commonPortConfig = os.path.join(hwSkuPath, "port_config.ini")
+   if os.path.exists(commonPortConfig):
+      portConfigPathList.append(commonPortConfig)
+   else:
+      files = os.listdir(hwSkuPath)
+      for fname in files:
+         fpath = os.path.join(hwSkuPath, fname)
+         if fname.isdigit() and os.path.isdir(fpath):
+            fileName = os.path.join(hwSkuPath, fname, "port_config.ini")
+            portConfigPathList.append(fileName)
+   return portConfigPathList
 
 def getPlatform():
    return platform.getPlatform()
