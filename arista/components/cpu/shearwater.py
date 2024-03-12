8920d26568a11dba6b7ebd1ae83b4d441a48a3d9
Samuel Angebault <staphylo@arista.com>
2024-03-12 14:19:06 +0100

    cpu: add Shearwater cpu support
    Change-Id: I3e7fb75d5e0a7df5fbc2574412d2de5370112d1c

---
+++ arista/components/cpu/shearwater.py
@@ -0,0 +1,33 @@
+
+from ...core.register import Register, RegBitField
+
+from ..cpld import SysCpld, SysCpldCommonRegisters
+
+from .lorikeet import LorikeetPrimeScdReloadCauseRegisters
+
+class ShearwaterSysCpldRegisters(SysCpldCommonRegisters):
+   PWR_CTRL_STS = Register(0x05,
+      RegBitField(7, 'pwrCtrl7', ro=False),
+      RegBitField(6, 'pwrCtrl6', ro=False),
+      RegBitField(5, 'pwrCtrl5', ro=False),
+      RegBitField(4, 'pwrCtrl4', ro=False),
+      RegBitField(3, 'pwrCtrl3', ro=False),
+      RegBitField(2, 'pwrCtrl2', ro=False),
+      RegBitField(1, 'pwrCtrl1', ro=False),
+      RegBitField(0, 'switchCardPowerGood'),
+   )
+   SCD_CTRL_STS = Register(0x0A,
+      RegBitField(6, 'scdInit', flip=True),
+      RegBitField(5, 'scdReset', ro=False),
+      RegBitField(4, 'scdHold', ro=False),
+      RegBitField(3, 'scdConfig', ro=False),
+      RegBitField(1, 'scdInitDone'),
+      RegBitField(0, 'scdConfDone'),
+   )
+   # TODO crc seu
+
+class ShearwaterSysCpld(SysCpld):
+   REGISTER_CLS = ShearwaterSysCpldRegisters
+
+class ShearwaterReloadCauseRegisters(LorikeetPrimeScdReloadCauseRegisters):
+   pass
---
+++ arista/platforms/cpu/shearwater.py
@@ -0,0 +1,73 @@
+from ...components.cpu.amd.k10temp import K10Temp
+from ...components.cpu.shearwater import (
+    ShearwaterReloadCauseRegisters,
+    ShearwaterSysCpld,
+)
+from ...components.lm75 import Tmp75
+from ...components.scd import Scd, ScdCause
+
+from ...core.cpu import Cpu
+from ...core.pci import PciRoot
+
+from ...descs.sensor import Position, SensorDesc
+
+class ShearwaterCpu(Cpu):
+
+   PLATFORM = 'shearwater'
+
+   def __init__(self, **kwargs):
+      super(ShearwaterCpu, self).__init__(**kwargs)
+
+      self.pciRoot = self.newComponent(PciRoot)
+
+      port = self.pciRoot.rootPort(device=0x18, func=3)
+      port.newComponent(K10Temp, addr=port.addr, sensors=[
+         SensorDesc(diode=0, name='Cpu temp sensor',
+                    position=Position.OTHER, target=70, overheat=95, critical=115),
+      ])
+
+      port = self.pciRoot.rootPort(device=0x18, func=7)
+      cpld = port.newComponent(Scd, addr=port.addr)
+      self.cpld = cpld
+
+      cpld.createPowerCycle()
+      cpld.addSmbusMasterRange(0x8000, 1, 0x80, 4)
+      cpld.newComponent(Tmp75, addr=cpld.i2cAddr(0, 0x48), sensors=[
+         SensorDesc(diode=0, name='Ambient',
+                    position=Position.OUTLET, target=55, overheat=75, critical=85),
+      ])
+
+      self.fanboard = self.parent.CHASSIS.addFanboard(cpld, cpld.getSmbus(7))
+
+      # TODO: cleanup syscpld declaration accross platforms
+      self.syscpld = self.newComponent(ShearwaterSysCpld, addr=cpld.i2cAddr(4, 0x23))
+      self.syscpld.addPowerCycle()
+
+      cpld.addReloadCauseProvider(causes=[
+         ScdCause(0x01, ScdCause.OVERTEMP),
+         ScdCause(0x08, ScdCause.REBOOT, 'Software Reboot'),
+         ScdCause(0x0a, ScdCause.POWERLOSS, 'PSU DC to CPU'),
+         ScdCause(0x0c, ScdCause.CPU),
+         ScdCause(0x0d, ScdCause.CPU_S3),
+         ScdCause(0x0e, ScdCause.CPU_S5),
+         ScdCause(0x20, ScdCause.RAIL, 'CPU_PWROK_3V3'),
+         ScdCause(0x21, ScdCause.RAIL, 'POSVDD_CPU_S0'),
+         ScdCause(0x22, ScdCause.RAIL, 'POSVDD_SOC_S0'),
+         ScdCause(0x23, ScdCause.RAIL, 'POS1V8_S0,POS3V3_S0'),
+         ScdCause(0x24, ScdCause.RAIL, 'POS0V6_VTT_MEM'),
+         ScdCause(0x25, ScdCause.RAIL, 'POS1V2_VDD_MEM'),
+         ScdCause(0x26, ScdCause.RAIL, 'POS2V5_VPP_MEM'),
+         ScdCause(0x27, ScdCause.RAIL, 'POS1V8'),
+         ScdCause(0x28, ScdCause.RAIL, 'POS0V9'),
+         ScdCause(0x29, ScdCause.RAIL, 'ALW_ON_PGOOD'),
+         ScdCause(0x2a, ScdCause.RAIL, 'ISL0_CAT_FAULT'),
+      ], regmap=ShearwaterReloadCauseRegisters,
+         priority=ScdCause.Priority.SECONDARY)
+
+   def getPciPort(self, num):
+      device, func = {
+         0: (0x3, 5),
+         2: (0x1, 3),
+      }[num]
+      bridge = self.pciRoot.pciBridge(device=device, func=func)
+      return bridge.downstreamPort(port=0)
